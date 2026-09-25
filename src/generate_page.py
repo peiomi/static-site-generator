fdcb1db44c6510f99c7593edf234284f4e3ce32d
peiomi <paydonriley@gmail.com>
2026-09-25 18:37:32 -0500

    baseurl path

--- src/generate_page.py
+++ src/generate_page.py
@@ -38,6 +38,8 @@ def generate_page(from_path, template_path, dest_path, basepath):
     if directory:
         os.makedirs(directory, exist_ok=True)
 
+    print("BASEPATH:", basepath)
+    print(html_page)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(html_page)
 
